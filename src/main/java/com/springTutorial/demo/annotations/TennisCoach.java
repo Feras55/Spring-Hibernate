97ec6fe6f2a6c0426e0bc98b215223c2240dc80a
Fares Gamal <ferasjamal55@gmail.com>
2022-09-13 17:23:52 +0200

    Section 8: Setter Injection
    Learned how to use perform Setter injection using java annotation (@Autowired), and about method Injection in general:
    1. Added Setter to inject dependency
    2. Tested method injection by refactoring the setter method's name into a non-conventional method

--- src/main/java/com/springTutorial/demo/annotations/TennisCoach.java
+++ src/main/java/com/springTutorial/demo/annotations/TennisCoach.java
@@ -10,10 +10,25 @@ public class TennisCoach implements Coach {
 
     //Injecting constructor Dependency using "Autowired"
     //which will look up the components for a component which implements FortuneService Interface
-    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
+
+
+    public TennisCoach() {
+
     }
+
+    //Injecting Setter Dependency
+    //extra: It is not obligatory that the setter function should be called the conventional setter name,
+    //It could be given any name, thus any method with a "@Autowired" annotation will inject dependency
+    @Autowired
+    public void anyMethodNotNecessarilyASetter(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+        System.out.println("Inside Setter");
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice backhand Volley";
