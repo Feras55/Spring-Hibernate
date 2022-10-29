a4b5327eaece7294c100b163e313b2d37173ebd2
Fares Gamal <ferasjamal55@gmail.com>
2022-10-29 17:50:08 +0200

    Section 13: Request Params and Request Mapping
    Learned how to request parameter and retrieve parameters from request, and learned about Controller Level mapping
    
    1. Retrieved parameter from request in two ways: a. using request.getParameter,
    b.  directly using @RequestParam in the method's parameter
    
    2. Did Controller Level Mapping to avoid ambiguous mapping

--- section11/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ section11/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -6,7 +6,7 @@
 <h2>Home Page - Spring MVC DEMO</h2>
 
 <hr>
-<a href="showForm">Hello World Form</a>
+<a href="hello/showForm">Hello World Form</a>
 </body>
 
 </html>
--- section11/spring-mvc-demo/src/com/springdemo/mvc/HelloWorldController.java
+++ section11/spring-mvc-demo/src/com/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello") //controller level mapping, all methods mapping is now relative to the controller mapping eg (/hello/showForm
 public class HelloWorldController {
 	
 	//need a controller method to show initial HTML form
@@ -41,5 +43,28 @@ public class HelloWorldController {
 		
 		return "helloworld";
 	}
+	
+	
+	//same method as above except to the way the name parameter is retrieved from the request
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String name, Model model) { 	//assign name to value of request parameter
+		
+	
+				
+		//convert data to uppercase
+		name = name.toUpperCase();
+		
+		//create message
+		String message = "Hey Bro! from V3 " + name;
+		//add message to the model
+		
+		model.addAttribute("message", message);
+		
+		return "helloworld";
+	}
+	
+
+	
 
 }
+
