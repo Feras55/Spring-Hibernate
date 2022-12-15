fe0cdbdf9bad7d58e46d6ea1594e0d280767b250
Fares Gamal <ferasjamal55@gmail.com>
2022-12-15 14:29:27 +0200

    Tested Lazy Fetching
    - Tested using DEBUG mode, saw that hibernate session queried courses when demanded only

--- eclipse Workspace/hb_eager_vs_lazy_demo/src/com/hibernate/demo/entity/Instructor.java
+++ eclipse Workspace/hb_eager_vs_lazy_demo/src/com/hibernate/demo/entity/Instructor.java
@@ -53,7 +53,7 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 	
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "instructor",
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "instructor",
 			   cascade = {CascadeType.PERSIST, CascadeType.DETACH, 
 					      CascadeType.MERGE, CascadeType.REFRESH})
 	private List<Course> courses;
