bf3cb8252dbe5398a26cf8b95bb3437e214e8794
Fares Gamal <ferasjamal55@gmail.com>
2022-12-15 14:10:50 +0200

    Section 24: CRUDS
    - Learned about one to many bi-directional mapping
    -Created Course Entity, associated it with the Instructor Class
    - Retrieved and deleted courses

--- eclipse Workspace/hb_03_one_to_many/src/com/hibernate/demo/entity/Instructor.java
+++ eclipse Workspace/hb_03_one_to_many/src/com/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -48,6 +52,19 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 	
+	@OneToMany(mappedBy = "instructor",
+			   cascade = {CascadeType.PERSIST, CascadeType.DETACH, 
+					      CascadeType.MERGE, CascadeType.REFRESH})
+	private List<Course> courses;
+	
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
 	public Instructor() {
 		
 	}
@@ -102,6 +119,19 @@ public class Instructor {
 	public String toString() {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+	
+	
+	//add convenience method for bi-directional relationship
+	public void add(Course tempCourse) {
+		
+		if(courses == null) {
+			courses = new ArrayList<>();
+		}
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
 	}
 	
 	
